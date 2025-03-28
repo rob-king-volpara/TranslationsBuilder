84ac4935c0022801f8f9ca64f5b282a560433218
Rob King <rob.king@volparahealth.com>
2025-03-28 14:41:11 +1300

    Refactor code for improved readability and structure
    This commit includes significant refactoring across multiple files to enhance code readability and maintainability. Key changes involve reorganizing namespaces, removing unnecessary using directives, and ensuring consistent formatting.

--- TranslationsBuilder/Forms/FormEditLocalizedLabel.cs
+++ TranslationsBuilder/Forms/FormEditLocalizedLabel.cs
@@ -1,40 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TranslationsBuilder.Services;
 
-namespace TranslationsBuilder {
+namespace TranslationsBuilder
+{
+    public partial class FormEditLocalizedLabel : Form
+    {
+        private string OriginalLabelName;
 
-  public partial class FormEditLocalizedLabel : Form {
+        public FormEditLocalizedLabel(string LabelName)
+        {
+            InitializeComponent();
+            OriginalLabelName = LabelName;
+            txtLabelName.Text = LabelName;
+        }
 
-    private string OriginalLabelName;
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            TranslationsManager.UpdateLocalizedLabel(OriginalLabelName, txtLabelName.Text);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
 
-    public FormEditLocalizedLabel(string LabelName) {
-      InitializeComponent();
-      OriginalLabelName = LabelName;
-      txtLabelName.Text = LabelName;    
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            TranslationsManager.DeleteLocalizedLabel(OriginalLabelName);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-
-    private void btnUpdate_Click(object sender, EventArgs e) {
-      TranslationsManager.UpdateLocalizedLabel(OriginalLabelName, txtLabelName.Text);
-      this.DialogResult= DialogResult.OK;
-      this.Close();
-    }
-
-    private void btnDelete_Click(object sender, EventArgs e) {
-      TranslationsManager.DeleteLocalizedLabel(OriginalLabelName);
-      this.DialogResult = DialogResult.OK;
-      this.Close();
-    }
-
-    private void btnCancel_Click(object sender, EventArgs e) {
-
-    }
-  }
 }
--- TranslationsBuilder/Forms/FormLoadingStatus.cs
+++ TranslationsBuilder/Forms/FormLoadingStatus.cs
@@ -1,35 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using TranslationsBuilder.Models;
 
-namespace TranslationsBuilder {
+namespace TranslationsBuilder
+{
+    public partial class FormLoadingStatus : Form, IStatusCalback
+    {
+        public void updateLoadingStatus(string TranslationType, string ObjectName, string OriginalText, string TranslatedText)
+        {
+            lblTranslationType.Text = TranslationType;
+            lblObjectName.Text = ObjectName;
+            lblOriginalText.Text = OriginalText;
+            lblTranslatedText.Text = TranslatedText;
+            RedrawUI();
+        }
 
-  public partial class FormLoadingStatus : Form, IStatusCalback {
+        public FormLoadingStatus()
+        {
+            InitializeComponent();
+            RedrawUI();
+        }
 
-    public void updateLoadingStatus(string TranslationType, string ObjectName, string OriginalText, string TranslatedText) {
-      lblTranslationType.Text = TranslationType;
-      lblObjectName.Text = ObjectName;
-      lblOriginalText.Text = OriginalText;
-      lblTranslatedText.Text = TranslatedText;
-      RedrawUI();
+        private void RedrawUI()
+        {
+            foreach (Control control in this.Controls)
+            {
+                control.Refresh();
+            }
+        }
     }
-
-    public FormLoadingStatus() {
-      InitializeComponent();
-      RedrawUI();
-    }
-
-    private void RedrawUI() {
-      foreach (Control control in this.Controls) {
-        control.Refresh();
-      }
-    }
-  }
 }
